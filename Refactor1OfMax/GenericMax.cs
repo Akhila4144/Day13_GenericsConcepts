805ef16e7e19734cdbb88ba845c36f1147b73234
Akhila4144 <orugantiakhila34@gmail.com>
2021-12-07 17:20:33 +0530

    Refactor Done

---
+++ Refactor1OfMax/GenericMax.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Refactor1OfMax
+{
+    class GenericMax<T> where T : IComparable
+    {
+        public T n1, n2, n3;
+        public GenericMax(T n1, T n2, T n3)
+        {
+            this.n1 = n1;
+            this.n2 = n2;
+            this.n3 = n3;
+        }
+        public static T MaxValue(T n1, T n2, T n3)
+        {
+            if (n1.CompareTo(n2) > 0 && n1.CompareTo(n3) > 0 || n1.CompareTo(n2) >= 0 && n1.CompareTo(n3) > 0 || n1.CompareTo(n2) > 0 && n1.CompareTo(n3) > 0)
+            {
+                Console.WriteLine(n1 + " Greater than " + n2 + " and " + n3);
+            }
+            else if (n2.CompareTo(n1) > 0 && n2.CompareTo(n3) > 0 || n2.CompareTo(n1) >= 0 && n2.CompareTo(n3) > 0 || n2.CompareTo(n1) > 0 && n2.CompareTo(n3) > 0)
+            {
+                Console.WriteLine(n2 + " Greater than " + n1 + " and " + n3);
+            }
+            else if (n3.CompareTo(n1) > 0 && n3.CompareTo(n2) > 0 || n3.CompareTo(n1) >= 0 && n3.CompareTo(n2) > 0 || n3.CompareTo(n1) > 0 && n3.CompareTo(n2) > 0)
+            {
+                Console.WriteLine(n3 + " Greater than " + n1 + " and " + n2);
+            }
+            return default;
+        }
+    }
+}
---
+++ Refactor1OfMax/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Refactor1OfMax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            GenericMax<int>.MaxValue(10, 2, 4);
+            GenericMax<double>.MaxValue(10.5, 22.5, 14.6);
+            GenericMax<String>.MaxValue("apple", "peach", "banana");
+        }
+    }
+}
