cf5418e91913d31b26347e814beb0c2a7b9bb52a
Akhila4144 <orugantiakhila34@gmail.com>
2021-12-07 17:15:06 +0530

    UC2 Done

---
+++ MaxFloatingNum/CompareNum.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MaxFloatingNum
+{
+    class CompareNum
+    {
+        public static void compareNo()
+        {
+            double x = 1.2, y = 2.4, z = 0.9;
+
+            if (x.CompareTo(y) > 0 && x.CompareTo(z) > 0 || x.CompareTo(y) >= 0 && x.CompareTo(z) > 0 || x.CompareTo(y) > 0 && x.CompareTo(z) >= 0)
+            {
+                Console.WriteLine(x + " Greater than " + y + " and " + z);
+            }
+            else if (y.CompareTo(x) > 0 && y.CompareTo(z) > 0 || y.CompareTo(x) >= 0 && y.CompareTo(z) > 0 || y.CompareTo(x) > 0 && y.CompareTo(z) >= 0)
+            {
+                Console.WriteLine(y + " Greater than " + x + " and " + z);
+            }
+            else if (z.CompareTo(x) > 0 && z.CompareTo(y) > 0 || z.CompareTo(x) >= 0 && z.CompareTo(y) > 0 || z.CompareTo(x) > 0 && z.CompareTo(y) >= 0)
+            {
+                Console.WriteLine(z + " Greater than " + x + " and " + y);
+            }
+        }
+    }
+
+}
+
---
+++ MaxFloatingNum/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MaxFloatingNum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("compared with each other to show the Maximum floating Number.");
+            Console.WriteLine("First Number =1.2 , Second Number =2.4 , Third Number = 0.9");
+            Console.WriteLine("-------------------------------------------------------------------------------------------");
+            CompareNum.compareNo();
+        }
+    }
+}
