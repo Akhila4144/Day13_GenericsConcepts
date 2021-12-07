5798a59e23d78f69ea25bb686641c23391464b2b
Akhila4144 <orugantiakhila34@gmail.com>
2021-12-07 17:07:54 +0530

    TC_1.1 Done

---
+++ MaxNumAt2ndPosition/CompareNum.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MaxNumAt2ndPosition
+{
+    class CompareNum
+    {
+        public static void MaxNum()
+        {
+            int x = 13, y = 45, z = 22;
+
+            if (x.CompareTo(y) > 0 && x.CompareTo(z) > 0 || x.CompareTo(y) >= 0 && x.CompareTo(z) > 0 || x.CompareTo(y) > 0 && x.CompareTo(z) >= 0)
+            {
+                Console.WriteLine(x + " Greater than " + y + " and " + z);
+            }
+            else if (y.CompareTo(x) > 0 && y.CompareTo(z) > 0 || y.CompareTo(x) >= 0 && y.CompareTo(z) > 0 || y.CompareTo(x) > 0 && y.CompareTo(z) >= 0)
+            {
+                Console.WriteLine(y + " Greater than " + x + " and " + z);
+            }
+            else if (z.CompareTo(x) > 0 && z.CompareTo(y) > 0 || z.CompareTo(x) >= 0 && z.CompareTo(y) > 0 || z.CompareTo(x) > 0 && z.CompareTo(y) >= 0)
+            {
+                Console.WriteLine(z + " Greater than " + x + " and " + y);
+            }
+        }
+    }
+}
+
---
+++ MaxNumAt2ndPosition/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MaxNumAt2ndPosition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("compared with each other to show the Maximum Number.");
+            Console.WriteLine("First Number = 13, Second Number = 45, Third Number = 22");
+            Console.WriteLine("-------------------------------------------------------------------------------------------");
+            CompareNum.MaxNum();
+            Console.WriteLine("After Comparision the 2nd postion Will be the Max Num");
+            Console.WriteLine("----------------------------------------------------------------------");
+        }
+    }
+}
