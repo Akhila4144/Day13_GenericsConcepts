6e1fe6efbad72a321b2ebb856d10768ff3ed1572
Akhila4144 <orugantiakhila34@gmail.com>
2021-12-08 14:21:48 +0530

    Printing Array DoneUsing Generics

---
+++ WithGenerics/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace WithGenerics
+{
+    class Program
+    {
+        public static void toPrint<T>(T[] inputArray)
+        {
+            foreach (var element in inputArray)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("---------");
+        }
+
+        
+        static void Main(string[] args)
+        {
+            int[] intArray = { 1, 2, 3, 4, 5 };
+            double[] doubleArray = {0.1,0.2,0.3 };
+            char[] charArray = { 'A','R'};
+
+            Program.toPrint<int>(intArray);
+            Program.toPrint<double>(doubleArray);
+            Program.toPrint<char>(charArray);
+
+        }
+        
+    }
+}
